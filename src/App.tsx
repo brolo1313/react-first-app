1b26180171f7d999d60f0cf43c12fad643f0b485
brolo1313 <brolo1341@gmail.com>
2022-04-28 14:23:13 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import bio from  './bio.json'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My bio:</h1>
+        <p>{bio.firstName}</p>
+        <p>{bio.bio}</p>
+        <p>{bio.contacts}</p>
       </header>
     </div>
   );
